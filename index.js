1e445f5479e586ea74b6288affff7cabe8db93a5
Bill Monney <billyemmel@gmail.com>
2018-01-16 20:03:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 function shout(string) {
-  string.toUpperCase()
+ return string.toUpperCase()
 }
+// returns string in caps
 
 function whisper(string) {
-  string.toLowerCase()
+ return string.toLowerCase()
 }
+// returns string in lower case
 
+function logShout(string) {
+  console.log(${string}.toUpperCase)
+}
+// prints string in all caps to console
+
+function logWhisper(string) {
+  console.log(${string}.toLowerCase)
+}
+//prints string in lower case to console
+
+function sayHiToGrandma(string) {
+  var uppercase = string.toUpperCase
+  var lowercase = string.toLowerCase
+
+if (string === uppercase) {
+    return "YES INDEED"
+  }
+ else if (string === lowercase) {
+    return "I can't hear you!"
+  }
+  
+}
